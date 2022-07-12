193175660563f1bf240572b31ab52405e2108d74
HeribertoJunior <hsc.jr94@gmail.com>
2022-07-12 10:37:43 -0300

    teste de lista

---
+++ lib/Models/planeta.dart
@@ -0,0 +1,7 @@
+class Planeta {
+  String nome;
+  String descricao;
+  String imagem;
+
+  Planeta(this.nome, this.descricao, this.imagem);
+}
--- lib/Screens/tela_inicial.dart
+++ lib/Screens/tela_inicial.dart
@@ -1,23 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:projeto5/Models/planeta.dart';
 import 'package:projeto5/widgets/card_planeta.dart';
 
 class TelaInicial extends StatelessWidget {
-  const TelaInicial({Key? key}) : super(key: key);
+  TelaInicial({Key? key}) : super(key: key);
 //CardPlaneta(
   //        descricao: 'Planeta dos Aneis',
-    //      imagem: 'saturno.png',
-      //    nome: 'Saturno',
-       // ),
+  //      imagem: 'saturno.png',
+  //    nome: 'Saturno',
+  // ),
+  var planetas = [
+    Planeta('Saturno', 'Planeta dos Aneis', 'saturno.png'),
+    Planeta('Saturno', 'Planeta dos Aneis', 'saturno.png'),
+    Planeta('Saturno', 'Planeta dos Aneis', 'saturno.png'),
+    Planeta('Saturno', 'Planeta dos Aneis', 'saturno.png'),
+    Planeta('Saturno', 'Planeta dos Aneis', 'saturno.png'),
+    Planeta('Saturno', 'Planeta dos Aneis', 'saturno.png'),
+    Planeta('Saturno', 'Planeta dos Aneis', 'saturno.png'),
+    Planeta('Saturno', 'Planeta dos Aneis', 'saturno.png'),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Pagina inicial")),
       body: Container(
-        child: ListView.builder(itemBuilder: (BuildContext context, int index ){
-          
-        }),
+        child: ListView.builder(
+            itemCount: planetas.length,
+            itemBuilder: (BuildContext context, int index) {
+              return CardPlaneta(
+                  nome: planetas[index].nome,
+                  descricao: planetas[index].descricao,
+                  imagem: planetas[index].imagem);
+            }),
       ),
     );
   }
